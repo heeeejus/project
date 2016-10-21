aacddff4073627f0647651977dc3b46bb849dbed
김희주 <mary2946@naver.com>
2016-10-21 10:30:35 +0900

    1014연습

---
+++ string/src/string/Ch6Ex2.java
@@ -0,0 +1,21 @@
+package string;
+
+public class Ch6Ex2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "문자열";
+		for(int i = 0; i<50 ; i++){
+			str=str+i;
+		}
+		System.out.println(str);
+		StringBuffer sb=new StringBuffer();
+		sb.append("문자열");
+		
+		for(int i= 0; i < 50; i++){
+			sb.append(i);
+		}
+		System.out.println(sb.toString());
+	}
+
+}
